d031f1f455a0e9abe7d49a4f54c13fb81daad593
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-16 20:41:53 +0530

    Added: Ammount Won or Lost

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -5,44 +5,58 @@ import java.util.Random;
 public class GamblingSimulator {
 
 	/**
-	 * starting with a stake of $100 every day and bet $1 every game. Generate a
-	 * random in static As a Calculative Gambler if won or lost 50% of the stake,
-	 * would resign for the day
+	 * starting with a stake of $100 every day and bet $1 every game. As a
+	 * Calculative Gambler if won or lost 50% of the stake, would resign for the day
+	 * After 20 days of playing every day would like to know the total amount won or
+	 * lost.
 	 */
-	public static final int initialStake = 100;
-	public static final int stakeBet = 1;
-	public static final float percentage50 = (initialStake / 100) * 50;
-	public static final float highestStake = percentage50 + initialStake;
-	public static final float lowestStake = percentage50 - initialStake;
 
-	static Random random = new Random();
+	public static final int STAKE = 100;
+	public static final int BET = 1;
+	public static final int PERCENTAGE_50_STAKE = (STAKE / 100) * 50;
+	public static final int HIGH_LIMIT = STAKE + PERCENTAGE_50_STAKE;
+	public static final int LOW_LIMIT = STAKE - PERCENTAGE_50_STAKE;
+	public static final int START_DAY = 1;
+	public static final int MAX_DAY = 20;
+	public static final int STAKES_PER_DAY = 50;
+	public static final int START_DOLLARS = 0;
 
 	public static void main(String[] args) {
 
-		int totalStake = initialStake;
+		System.out.println("Welcome to the Gambler Game"); // print
+		// Initialize
+		int days = START_DAY;
+		int totalDollars = START_DOLLARS;
 
-		while (totalStake < highestStake && totalStake > lowestStake) {
-			int play = random.nextInt(2); // generate random number in range 0,1
+		for (days = START_DAY; days <= MAX_DAY; days++) {
+			int totalStake = STAKE;
 
-			switch (play) {
-
-			case 0:
-				totalStake = totalStake - stakeBet;
-				System.out.println("Stake after loosing: " + totalStake); // print result
-				break;
-
-			case 1:
-				totalStake = totalStake + stakeBet;
-				System.out.println("Stake after winning: " + totalStake);
-				break;
+			while (totalStake < HIGH_LIMIT && totalStake > LOW_LIMIT) {
+				int luck = (int) (Math.random() * 10) % 2;
+				if (luck == 1) {
+					totalStake = totalStake + BET;
+				} else {
+					totalStake = totalStake - BET;
+				}
 			}
 
-		}
-		// if will get the bet is won or lose
-		if (totalStake == highestStake)
-			System.out.println("Gambler won by: " + totalStake);
-		else
-			System.out.println("Gambler lost by: " + totalStake);
-	}
+			// It will get the dollars is win or lose
 
+			if (totalStake == HIGH_LIMIT) {
+				totalDollars += STAKES_PER_DAY;
+				System.out.println("Gambler won by " + totalDollars + " dollars on day " + days);
+			} else {
+				totalDollars -= STAKES_PER_DAY;
+				System.out.println("Gambler lost by " + totalDollars + " dollars on day " + days);
+			}
+		}
+		/**
+		 * if loop we have to check desired dollars and print result
+		 */
+		if (totalDollars > START_DOLLARS) {
+			System.out.println("Gambler won " + totalDollars);
+		} else {
+			System.out.println("Gambler lost " + totalDollars);
+		}
+	}
 }
