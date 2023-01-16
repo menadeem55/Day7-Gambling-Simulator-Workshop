b6d94588a5febd268b8441dd281704d014b0938c
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-16 18:57:10 +0530

    Added: welcome message

---
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.gamblingsimulator;
+
+public class GamblingSimulator {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the Gambling Simulator Game");
+
+	}
+
+}
