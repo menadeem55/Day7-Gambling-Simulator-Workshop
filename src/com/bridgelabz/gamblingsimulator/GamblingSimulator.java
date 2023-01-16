815295f3762ca54f67e46db74aeb8ed136cf47fb
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-16 19:15:29 +0530

    Added: functonality of bet per day and stake per day

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -2,11 +2,15 @@ package com.bridgelabz.gamblingsimulator;
 
 public class GamblingSimulator {
 
+	// starting with a stake of $100 every day and bet $1 every game.
+	public static final int initialStake = 100;
+	public static final int stakeBet = 1;
+
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to the Gambling Simulator Game");
-		
-		
+		System.out.println("Initial Stake is: " + initialStake + "$");
+		System.out.println("Stake Bet is " + stakeBet);
+
 	}
 
 }
